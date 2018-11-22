977e46dc9244373a9439eb7a0578731152558fc3
Caren Keyes <cejkeyes@gmail.com>
2018-11-22 13:39:58 -0800

    add bootstrap, scss, and custom css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap-scss';
+import './styles/custom.scss';
 import App from './containers/App/App';
 import * as serviceWorker from './serviceWorker';
 
