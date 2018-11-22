8bb8f7327d908211a02eb31c773c27a695fdd633
Caren Keyes <cejkeyes@gmail.com>
2018-11-22 10:04:01 -0800

    set up environments

---
+++ src/config/environments/production.js
@@ -0,0 +1,10 @@
+
+import defaultConfig from 'config/environments/default';
+
+const configuration = {
+    ...defaultConfig,
+    CONFIG_NAME: 'Production Config',
+    MAIN_APP_AUTHORITY: '',
+};
+
+export default configuration;
