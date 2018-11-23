0d49f0cf236f922d4d244d100cc306bf5668a3b1
Caren Keyes <cejkeyes@gmail.com>
2018-11-22 19:01:03 -0800

    initial commit

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,12 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
+import {Route, Switch, withRouter} from 'react-router-dom';
+import LandingPage from '../LandingPage/landing-page';
+import SearchPage from '../SearchPage/search-page';
+import ResourcePage from '../ResourcePage/resource-page';
+import ProfilePage from '../ProfilePage/profile-page';
+import FormPage from '../FormPage/form-page';
+import Navbar from '../Navbar/navbar';
+import Footer from '../Footer/footer';
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <h1>Make sure it works</h1>
-        <div className="test-css container primary" />
+      <div className="App container-fluid">
+          <Navbar />
+          <main role="main">
+            <Switch>
+              <Route exact path='/' component={LandingPage} />
+              <Route exact path='/search' component={SearchPage} />
+              <Route exact path='/resource' component={ResourcePage} />
+              <Route exact path='/profile' component={ProfilePage} />
+              <Route path='/form' component={FormPage} />
+            </Switch>
+          </main>
+          <Footer />
       </div>
     );
   }
