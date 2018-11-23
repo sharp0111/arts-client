024a061a02870b2c35a0ca05ff87c6f2fe0a3708
Caren Keyes <cejkeyes@gmail.com>
2018-11-22 20:44:42 -0800

    basic navbar styling

--- src/containers/Navbar/navbar.js
+++ src/containers/Navbar/navbar.js
@@ -3,7 +3,11 @@ import React from 'react';
 export default class Navbar extends React.Component{
     render(){
         return(
-            <nav className="navbar navbar-expand-md bg-dark"></nav>
+            <nav className='navbar navbar-expand-md bg-dark'>
+                <a className='navbar-brand' href=''>
+                    Arts Connective
+                </a>
+            </nav>
         )
     }
 }
