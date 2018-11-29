d0917aa26fefa625e14eeadd5819df4b3be7cfeb
Caren Keyes <cejkeyes@gmail.com>
2018-11-29 12:35:55 -0800

    add price section

---
+++ src/containers/AddResourceForm/price-section.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import Input from '../FormElements/input';
+import {Field, reduxForm} from 'redux-form';
+import {required, nonEmpty} from '../../validators';
+
+const PriceSection = props => {
+    const {handleSubmit, previousPage} = props;
+    return(
+        <form onSubmit={handleSubmit}>
+            <Field 
+                component={Input}
+                type='number'
+                label='Rate'
+                name='rate'
+                validate={[required, nonEmpty]}
+            />
+            <Field 
+                component={Input}
+                type='text'
+                label='Enter Unit'
+                name='unti'
+                validate={[required, nonEmpty]}
+            />
+            <Field 
+                component={Input}
+                type='textarea'
+                label='Additional'
+                name='additional'
+                validate={[required, nonEmpty]}
+            />
+            <div className='row form-navigation-buttons'>
+                <button type='button' className='form-back' onClick={previousPage}>
+                    Back
+                </button>
+                <button type='submit' className='form-next'>
+                    Next
+                </button>
+            </div>
+        </form>
+    )
+}
+
+export default reduxForm({
+    form: 'add-resource-form',
+    destroyOnUnmount: false,
+    forceUnregisterOnUnmount: false,
+})(PriceSection);
