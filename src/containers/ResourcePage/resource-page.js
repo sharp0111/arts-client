6ca8a5991f7a23abe7b762ff8c63c22041fe2949
Caren Keyes <cejkeyes@gmail.com>
2018-12-05 13:49:15 -0800

    initial commit

--- src/containers/ResourcePage/resource-page.js
+++ src/containers/ResourcePage/resource-page.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import{Switch, Route} from 'react-router-dom';
+import SelectedResourceWrapper from './selected-resource-wrapper';
+import CreatedResourceWrapper from './created-resource-wrapper';
 
 export default class ResourcePage extends React.Component{
     render(){
 
         return(
             <div className="container resource-page">
-                <p> Resource Page</p>
+                <Switch>
+                    <Route path='/resource/created' component={CreatedResourceWrapper} />
+                    <Route path='/resource/selected' component={SelectedResourceWrapper} />
+                </Switch>
             </div>
         )
     }
