565b3edb68508013837528b4c675255aba2275a5
Caren Keyes <cejkeyes@gmail.com>
2018-12-06 14:41:38 -0800

    initial commit

--- src/containers/FormPage/form-page.js
+++ src/containers/FormPage/form-page.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom';
 import AddResourceWrapper from '../AddResourceWrapper/add-resource-wrapper';
+import ImageUploadWrapper from '../ImageUpload/image-upload-wrapper';
 
 export default class FormPage extends React.Component{
     render(){
 
         return(
             <div className="container form-page">
-                <AddResourceWrapper />
+                <Switch>
+                    <Route path='/form/resource' component={AddResourceWrapper} />
+                    <Route path='/form/image' component={ImageUploadWrapper} />
+                </Switch>
             </div>
         )
     }
---
+++ src/containers/ImageUpload/image-upload-wrapper.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class ImageUploadWrapper extends React.Component{
+    render(){
+        return(
+            <div><h2>Image Wrapper</h2></div>
+        )
+    }
+}
