ff6f5330f1ceec5bd094508553cf22e47d03904d
Caren Keyes <cejkeyes@gmail.com>
2018-12-07 23:42:56 -0800

    add image carousel

---
+++ src/containers/ImageCarousel/imagecarousel.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Carousel} from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import {Image} from 'cloudinary-react';
+
+export default class ImageCarousel extends React.Component{
+    render(){
+        console.log(this.props.images)
+        let slides = this.props.images.map((image, index) => 
+            <div key={index}>
+                <Image cloudName='arts-connective' publicId={image} height='500' width='650' />
+            </div>
+        )
+        return(
+            <div className='contain-carousel'>
+                <Carousel autoPlay showThumbs={false}>
+                    {slides}
+                </Carousel>
+            </div>
+        )
+    }
+}
