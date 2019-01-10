ca7a7abdb37ea04a3a3cac4fca234160d031a47f
Caren Keyes <cejkeyes@gmail.com>
2019-01-10 12:11:03 -0800

    moved add resource to dashboard page

--- src/containers/Dashboard/dashboard-wrapper.js
+++ src/containers/Dashboard/dashboard-wrapper.js
@@ -1,19 +1,30 @@
+
 import React from 'react';
 import {connect} from 'react-redux';
+import {Link, Redirect} from 'react-router-dom';
 
 export class DashboardWrapper extends React.Component{
     render(){
-
+        console.log(this.props.loggedIn)
+        if(!this.props.loggedIn){
+            return(
+                <Redirect to='form/login' />
+            )
+        }
         return(
             <div className="container profile-page">
                 <p>Profile Page</p>
+                <Link to='form/resource'>
+                    + Add Resource
+                </Link>
             </div>
         )
     }
 }
 
 const mapStateToProps = state => ({
-    user: state.resource.user
+    loggedIn: state.user._id !== null,
+    user: state.user
 })
 
 export default connect(mapStateToProps)(DashboardWrapper)
