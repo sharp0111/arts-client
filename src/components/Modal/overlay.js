d5ef072a939da0ebfa8801a4fcfe18d5e5b802ea
Caren Keyes <cejkeyes@gmail.com>
2019-01-11 14:26:19 -0800

    initial commit, work on click outside functionality

--- src/components/Modal/overlay.js
+++ src/components/Modal/overlay.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import Modal from '../../containers/FormElements/modal';
 import './modal.css';
 
 
 export default function Overlay(props){
     return(
         <div className='overlay'>
-            <Modal {...props}/>
         </div>
     )
 }
